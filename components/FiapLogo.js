c0c8684e3029984b9af7ced5e3c6f426c3866151
lucksza <luccasaraivaborges@gmail.com>
2026-03-26 21:06:17 -0300

    feat: criar componente FiapLogo SVG

---
+++ components/FiapLogo.js
@@ -0,0 +1,40 @@
+import Svg, { Path } from 'react-native-svg';
+
+export default function FiapLogo({ width = 139, height = 37, color = '#FFFFFF' }) {
+  const scale = width / 139;
+  const scaledHeight = 37 * scale;
+
+  return (
+    <Svg
+      width={width}
+      height={height || scaledHeight}
+      viewBox="0 0 139.01 37.01"
+      fill="none"
+    >
+      <Path
+        d="M42.2744 0H39.5078V36.7432H42.2744V0Z"
+        fill={color}
+      />
+      <Path
+        d="M23.7546 17.5908H10.2891V20.116H23.7546V17.5908Z"
+        fill={color}
+      />
+      <Path
+        d="M0 0V36.7432H2.7666V2.52516H31.3404V0H0Z"
+        fill={color}
+      />
+      <Path
+        d="M125.194 0.149414H103.234V36.8926H106.001V23.1968H106.023V20.6717H106.001V2.67457H125C131.678 2.67457 136.239 5.71332 136.239 11.534V11.641C136.239 17.0979 131.57 20.6717 124.676 20.6717H115.987V23.1968H124.524C132.262 23.1968 139.006 19.2165 139.006 11.4912V11.3842C138.962 4.27955 133.343 0.149414 125.194 0.149414Z"
+        fill={color}
+      />
+      <Path
+        d="M87.172 20.6721L74.3116 0H71.6531L48.7422 37H51.5952L72.9283 2.99595L84.0596 20.6721H87.172Z"
+        fill={color}
+      />
+      <Path
+        d="M91.4051 27.4561H88.3359L94.323 37.0003H97.349L91.4051 27.4561Z"
+        fill={color}
+      />
+    </Svg>
+  );
+}
